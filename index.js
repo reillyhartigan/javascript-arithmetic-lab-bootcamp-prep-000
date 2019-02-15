78fa7999015fa1609c1c67395fc663f2a094a5c7
sensible-state-8093 <reilly.hartigan@yale.edu>
2019-02-15 00:43:54 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+var a, b
+
+a = 10
+b=5
+
+function add(a, b) {
+  return a + b
+}
+function subtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function divide(a,b) {
+  return a / b
+}
